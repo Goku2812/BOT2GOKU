43b74736e50f51dbe66b8986c1e794fcca4bf160
goku2812 <cayancela730@gmail.com>
2022-11-01 11:25:39 -0500

    iniciado

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+GREEN='\033[0;32m'
+while : 
+do
+echo "${GREEN} Goku Bot Esta Iniciando..... 
+Auto reconexión ativada para prevención de quedas.."
+    node index.js
+    sleep 1
+
+done
